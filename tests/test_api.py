9245d7835386c6c03ecf03199f9f8bcac06ac57f
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-12-07 20:30:33 +0800

    tests

---
+++ tests/test_api.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+
+
+def test_get_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_post_join(client):
+    response = client.post(
+        "/join",
+        json={"user_name": "test", "password": "test", "display_name": "asdf"}
+    )
+    assert response.status_code == 200
+
+
+def test_post_login(client):
+    client.post(
+        "/join",
+        json={"user_name": "test_login",
+              "password": "test111111", "display_name": "asdf"}
+    )
+    response = client.post(
+        "/login",
+        json={"user_name": "test_login", "password": "test111111"}
+    )
+    assert response.status_code == 200
+
+
+def test_get_admin_apps(client):
+    response = client.get("/admin/apps")
+    assert response.status_code == 200
+
+
+def test_post_admin_apps(client):
+    response = client.post("/admin/apps",
+                           json={
+                               "name": "hello",
+                               "image": "nginx:alpine"
+                           })
+    assert response.status_code == 200
+
+
+def test_post_admin_app_update(client):
+    creaded_app = client.post("/admin/apps",
+                              json={
+                                  "name": "hello_update",
+                                  "image": "nginx:alpine"
+                              }).json()
+    response = client.post("/admin/apps/{}".format(creaded_app.get("id")),
+                           json={"image": "nginx:alpine"})
+    assert response.status_code == 200
+
+
+def test_post_admin_app_delete(client):
+    creaded_app = client.post("/admin/apps",
+                              json={
+                                  "name": "hello_delete",
+                                  "image": "nginx:alpine"
+                              }).json()
+    response = client.post(
+        "/admin/apps/{}/delete".format(creaded_app.get("id")))
+    assert response.status_code == 200
+
+
+def test_get_admin_app_roles(client):
+    creaded_app = client.post("/admin/apps",
+                              json={
+                                  "name": "hello_roles",
+                                  "image": "nginx:alpine"
+                              }).json()
+    response = client.get("/admin/apps/{}/roles".format(creaded_app.get("id")))
+    assert response.status_code == 200
+
+
+def test_post_admin_app_roles(client):
+    creaded_app = client.post("/admin/apps",
+                              json={
+                                  "name": "hello_roles_create",
+                                  "image": "nginx:alpine"
+                              }).json()
+    url = "/admin/apps/{}/roles".format(creaded_app.get("id"))
+    response = client.post(url,
+                           json={
+                               "title": "string",
+                               "auth": {"query": "hello"},
+                               "description": "string",
+                               "enabled": True,
+                           }
+                           )
+
+    assert response.status_code == 200
+
+
+def test_post_admin_app_role_update(client):
+    creaded_app = client.post("/admin/apps",
+                              json={
+                                  "name": "hello_roles_update",
+                                  "image": "nginx:alpine"
+                              }).json()
+    url = "/admin/apps/{}/roles".format(creaded_app.get("id"))
+    created_role = client.post(url,
+                               json={
+                                   "title": "string",
+                                   "auth": {"query": "hello"},
+                                   "description": "string",
+                                   "enabled": True,
+                               }
+                               ).json()
+    response = client.post(url + "/{}".format(created_role.get("id")),
+                           json={
+                               "title": "role_title_updated",
+                               "auth": {"query": "helloworld"},
+                               "description": "string",
+                               "enabled": True,
+    }
+    )
+    assert response.status_code == 200
+    assert response.json().get('title') == "role_title_updated"
+
+
+def test_post_admin_app_role_delete(client):
+    creaded_app = client.post("/admin/apps",
+                              json={
+                                  "name": "hello_roles_delete",
+                                  "image": "nginx:alpine"
+                              }).json()
+    url = "/admin/apps/{}/roles".format(creaded_app.get("id"))
+    created_role = client.post(url,
+                               json={
+                                   "title": "string",
+                                   "auth": {"query": "hello"},
+                                   "description": "string",
+                                   "enabled": True,
+                               }
+                               ).json()
+    response = client.post(url + "/{}/delete".format(created_role.get('id')))
+    assert response.status_code == 200
+
+
+def test_get_admin_users(client):
+    response = client.get("/admin/users")
+    assert response.status_code == 200
+
+
+def test_post_admin_users(client):
+    response = client.post("/admin/users",
+                           json={
+                               "user_name": "created_user",
+                               "display_name": "string",
+                               "email": "string"
+                           })
+    created_user = response.json()
+    assert response.status_code == 200
+    assert created_user.get('user_name') == "created_user"
+
+
+def test_get_admin_user(client):
+    creaded_user = client.post("/admin/users",
+                               json={
+                                   "user_name": "created_user2",
+                                   "display_name": "string",
+                                   "email": "string"
+                               }).json()
+    response = client.get("/admin/users/{}".format(creaded_user.get("id")))
+    find_user = response.json()
+    assert response.status_code == 200
+    assert find_user.get('user_name') == "created_user2"
+
+
+def test_post_admin_user(client):
+    creaded_user = client.post("/admin/users",
+                               json={
+                                   "user_name": "created_user3",
+                                   "display_name": "string",
+                                   "email": "string"
+                               }).json()
+    response = client.post("/admin/users/{}".format(creaded_user.get("id")),
+                           json={
+                               "user_name": "created_user3_updated",
+                               "display_name": "string_updated",
+    })
+    find_user = response.json()
+    assert response.status_code == 200
+    assert find_user.get('user_name') == "created_user3_updated"
+
+
+def test_post_admin_user_delete(client):
+    creaded_user = client.post("/admin/users",
+                               json={
+                                   "user_name": "created_user4",
+                                   "display_name": "string",
+                                   "email": "string"
+                               }).json()
+    response = client.post(
+        "/admin/users/{}/delete".format(creaded_user.get("id")))
+    delete_user = response.json()
+    assert response.status_code == 200
+    assert delete_user.get('user_name') == "created_user4"
+
+
+def test_post_admin_user_authorized(client):
+    creaded_app = client.post(
+        "/admin/apps",
+        json={
+            "name": "hello_user_authorized",
+            "image": "nginx:alpine"
+        }).json()
+    url = "/admin/apps/{}/roles".format(creaded_app.get("id"))
+    created_role = client.post(
+        url,
+        json={
+            "title": "string",
+            "auth": {"query": "hello"},
+            "description": "string",
+            "enabled": True,
+        }
+    ).json()
+    response = client.post(
+        "/admin/users/{}/authorized/apps/{}".format(1, creaded_app.get("id")),
+        json=[created_role.get("id")])
+    authorized = response.json()
+    assert response.status_code == 200
+    assert created_role.get("id") in ([item.get('id') for item in authorized[0].get('roles')])
---
+++ tests/test_crud.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+from app.services.database import crud
+from app import schemas
+
+
+def test_app_create(db):
+    app = crud.app.create(db, payload=schemas.AppCreate.parse_obj(
+        {
+            'name': 'admin',
+            'description': 'admin',
+            'image': 'nginx:alpine',
+            'ingress': [
+                {
+                    'name': 'login',
+                    'path': '/login',
+                    'target': {'path': '/login', 'port': 80},
+                },
+                {
+                    'name': 'join',
+                    'path': '/join',
+                    'target': {'path': '/join', 'port': 80},
+                },
+                {
+                    'name': 'index',
+                    'path': '/',
+                    'target': {'path': '/', 'port': 80},
+                }
+            ],
+            'depends': [
+                {
+                    'name': 'redis',
+                    'title': 'redis',
+                    'image': 'redis:alpine',
+                },
+                {
+                    'name': 'gateway',
+                    'title': 'gateway',
+                    'image': 'mblocks/gateway',
+                    'ingress': [
+                        {
+                            'name': 'api',
+                            'path': '/api',
+                            'target': {
+                                'path': '/',
+                                'port': 8000,
+                            }
+                        },
+                    ],
+                    'environment': [
+                        {'name': 'KONG_NGINX_HTTP_INCLUDE',
+                         'value': '/usr/local/kong/nginx-custom.conf'},
+                        {'name': 'LOCALDOMAIN', 'value': '127.0.0.11'},
+                        {'name': 'KONG_DATABASE', 'value': 'off'},
+                        {'name': 'KONG_PROXY_ACCESS_LOG',
+                         'value': '/dev/stdout'},
+                        {'name': 'KONG_PROXY_ERROR_LOG',
+                         'value': '/dev/stdout'},
+                        {'name': 'KONG_ADMIN_ACCESS_LOG',
+                         'value': '/dev/stdout'},
+                        {'name': 'KONG_ADMIN_ERROR_LOG',
+                         'value': '/dev/stdout'},
+                        {'name': 'KONG_ADMIN_LISTEN',
+                         'value': '0.0.0.0:8001'},
+                        {'name': 'KONG_PLUGINS',
+                         'value': 'bundled,redis-auth'},
+                        {'name': 'REDIS_HOST',
+                         'value': 'redis.admin.mblocks'},
+                    ]
+                },
+                {
+                    'name': 'traefik',
+                    'image': 'traefik:v2.4',
+                    'ports': [
+                        {'name': 'http', 'container_port': 8080, 'host_port': 8080},
+                        {'name': 'http', 'container_port': 80, 'host_port': 80}
+                    ],
+                    'volumes': [
+                        {
+                            'name': 'docker',
+                            'mount_path': '/var/run/docker.sock',
+                            'host_path': '/var/run/docker.sock',
+                            '/var/run/docker.sock': '/var/run/docker.sock'
+                        },
+                    ],
+                    'command': [
+                        "--api.insecure=true",
+                        "--providers.docker=true",
+                        "--providers.docker.exposedbydefault=false",
+                        "--entrypoints.web.address=:80"
+                    ]
+                }
+            ]
+        })
+    )
+    find_app = crud.app.get(db=db, filter={'id': app.id})
+    assert find_app.name == 'admin'
+
+
+def test_app_update(db):
+    app = crud.app.create(db, payload=schemas.AppCreate.parse_obj(
+        {
+            'name': 'test_app_update',
+            'description': 'admin',
+            'image': 'nginx:alpine',
+        }))
+    crud.app.update(db,
+                    filter={'id': app.id},
+                    payload={
+                        'name': 'test_app_update_updated',
+                        'description': 'admin_updated',
+                        'image': 'nginx:alpine_admin_updated',
+                        'environment': [
+                            {
+                                'name': 'Hello',
+                                'value': 'world'}
+                        ]
+                    })
+    find_app = crud.app.get(db=db, filter={'id': app.id})
+    assert find_app.name == 'test_app_update_updated'
+    assert find_app.description == 'admin_updated'
+    assert find_app.image == 'nginx:alpine_admin_updated'
+    assert find_app.environment[0].get('name') == 'Hello'
+
+
+def test_app_delete(db):
+    app = crud.app.create(db, payload=schemas.AppCreate.parse_obj(
+        {
+            'name': 'test_app_delete',
+            'description': 'admin',
+            'image': 'nginx:alpine',
+        }))
+    crud.app.delete(db, filter={'id': app.id})
+    find_app = crud.app.get(db=db, filter={'id': app.id})
+    assert find_app == None
+
+
+def test_account_create(db):
+    account = crud.account.create(db, payload=schemas.AccountCreate.parse_obj({
+        'user_name': 'test_account_create',
+        'display_name': 'hello',
+        'password': '123456789'
+    }))
+
+    find_account = crud.account.get(db=db, filter={'id': account.id})
+    assert find_account.user_name == account.user_name
+
+
+def test_account_update(db):
+    account = crud.account.create(db, payload=schemas.AccountCreate.parse_obj({
+        'user_name': 'test_account_update',
+        'display_name': 'hello',
+        'password': '123456789'
+    }))
+
+    updated_account = crud.account.update(db=db, filter={'id': account.id}, payload={
+                                          'display_name': 'hello_updated'})
+    assert updated_account.display_name == 'hello_updated'
+
+
+def test_user_create(db):
+    user = crud.user.create(db, payload=schemas.UserCreate.parse_obj({
+        'user_name': 'test_user_create',
+        'display_name': 'hello',
+        'password': '123456789'
+    }))
+
+    find_user = crud.user.get(db=db, filter={'id': user.id})
+    assert find_user.user_name == user.user_name
+
+
+def test_user_update(db):
+    user = crud.user.create(db, payload=schemas.UserCreate.parse_obj({
+        'user_name': 'test_user_update',
+        'display_name': 'hello',
+        'password': '123456789'
+    }))
+
+    updated_user = crud.user.update(db=db, filter={'id': user.id}, payload={
+                                    'display_name': 'hello_updated'})
+    assert updated_user.display_name == 'hello_updated'
+
+
+def test_role_create(db):
+    role = crud.role.create(db, payload=schemas.RoleCreate.parse_obj({
+        'title': 'test_role_create',
+        'auth': {"query":{}},
+        'parent_id': 1,
+    }))
+
+    find_role = crud.role.get(db=db, filter={'id': role.id})
+    assert find_role.title == role.title
+
+
+def test_role_update(db):
+    role = crud.role.create(db, payload={
+        'title': 'test_role_update',
+        'auth': {"query": {"hello": 1000}},
+    })
+
+    updated_role = crud.role.update(db=db,
+                                    filter={'id': role.id},
+                                    payload={
+                                        'title': 'test_role_updated',
+                                        'auth': {"query": {"hello": 2000,"world":'21'}},
+                                    })
+    assert updated_role.title == 'test_role_updated'
+
+
+def test_authorized_set(db):
+    created_app = crud.app.create(db, payload=schemas.AppCreate.parse_obj(
+        {
+            'name': 'test_app_authorized',
+            'description': 'admin',
+            'image': 'nginx:alpine',
+        }))
+    created_role = crud.role.create(db, payload={
+        'title': 'test_role_update',
+        'auth': {"query": {"hello": 1000}},
+        'app_id': created_app.id
+    })
+    created_authorized = crud.authorized.create(db, payload=schemas.AuthorizedCreate(app_id=created_app.id,user_id=1,roles=[created_role.id]))
+    assert len(created_authorized.get('roles')) == 1
